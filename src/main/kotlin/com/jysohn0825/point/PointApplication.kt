3f6bc28c73b95a74a29680825b76cdfb17e92803
jys <jysohn0825@gmail.com>
2026-07-21 21:01:12 +0900

    init

---
+++ src/main/kotlin/com/jysohn0825/point/PointApplication.kt
@@ -0,0 +1,11 @@
+package com.jysohn0825.point
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PointApplication
+
+fun main(args: Array<String>) {
+    runApplication<PointApplication>(*args)
+}
---
+++ src/test/kotlin/com/jysohn0825/point/PointApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jysohn0825.point
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PointApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
