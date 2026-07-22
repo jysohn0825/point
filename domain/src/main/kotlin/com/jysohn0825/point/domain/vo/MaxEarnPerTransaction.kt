2786911d2aff3781dcaf9d1af6c3d892753ee487
jys <jysohn0825@gmail.com>
2026-07-22 21:49:03 +0900

    feat(domain): PointPolicy 정책 값 객체(VO) 추가
    1회 적립 상한, 개인 보유한도, 기본 만료일을 하드코딩 없이
    관리할 수 있도록 각 값을 검증하는 VO를 도입한다.

---
+++ domain/src/main/kotlin/com/jysohn0825/point/domain/vo/MaxEarnPerTransaction.kt
@@ -0,0 +1,15 @@
+package com.jysohn0825.point.domain.vo
+
+data class MaxEarnPerTransaction(val value: Long) {
+
+    init {
+        require(value in MIN..MAX) {
+            "1회 적립 가능 포인트는 ${MIN}포인트 이상 ${MAX}포인트 이하이어야 합니다. 입력값: $value"
+        }
+    }
+
+    companion object {
+        const val MIN: Long = 1
+        const val MAX: Long = 100_000
+    }
+}
---
+++ domain/src/main/kotlin/com/jysohn0825/point/domain/vo/MaxHoldingAmount.kt
@@ -0,0 +1,14 @@
+package com.jysohn0825.point.domain.vo
+
+data class MaxHoldingAmount(val value: Long) {
+
+    init {
+        require(value >= MIN) {
+            "개인별 보유 가능한 무료 포인트의 최대 금액은 ${MIN}포인트 이상이어야 합니다. 입력값: $value"
+        }
+    }
+
+    companion object {
+        const val MIN: Long = 1
+    }
+}
