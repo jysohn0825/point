42fcc4a923338c099d11edf8ee2bb5c3275c3825
jys <jysohn0825@gmail.com>
2026-07-22 21:52:12 +0900

    feat(point-wallet): 포인트 지갑 값 객체 추가
    Balance, HoldingLimit, PointAmount를 불변 값 객체(@JvmInline value class)로
    도입해 잔액/보유한도/적립금액의 원시 타입 오용을 방지하고, 각 VO 생성
    시점에 도메인 불변식(음수 잔액 금지, 0 이하 한도/금액 금지)을 강제한다.

---
+++ domain/src/main/kotlin/com/jysohn0825/point/domain/vo/HoldingLimit.kt
@@ -0,0 +1,11 @@
+package com.jysohn0825.point.domain.vo
+
+@JvmInline
+value class HoldingLimit(val value: Long) {
+    init {
+        require(value > 0) { "보유한도는 0보다 커야 합니다." }
+    }
+
+    fun canAccept(balance: Balance, pointAmount: PointAmount): Boolean =
+        balance.amount + pointAmount.value <= value
+}
