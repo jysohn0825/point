b912e131c12146a7d81f3d0bc9afce1c16a68684
jys <jysohn0825@gmail.com>
2026-07-22 21:49:07 +0900

    feat(domain): PointPolicy 루트 엔티티 추가
    정책 값(VO)을 감싸는 PointPolicy 엔티티를 추가한다. 관리자가
    값을 갱신할 수 있도록 update()를 제공하고, id 기준 동일성을
    보장하도록 equals/hashCode를 재정의한다.

---
+++ domain/src/main/kotlin/com/jysohn0825/point/domain/entity/PointPolicy.kt
@@ -0,0 +1,40 @@
+package com.jysohn0825.point.domain.entity
+
+import com.jysohn0825.point.domain.vo.DefaultExpirationPeriod
+import com.jysohn0825.point.domain.vo.MaxEarnPerTransaction
+import com.jysohn0825.point.domain.vo.MaxHoldingAmount
+import java.util.UUID
+
+class PointPolicy(
+    val id: String = UUID.randomUUID().toString(),
+    maxEarnPerTransaction: MaxEarnPerTransaction,
+    maxHoldingAmount: MaxHoldingAmount,
+    defaultExpirationPeriod: DefaultExpirationPeriod,
+) {
+    var maxEarnPerTransaction: MaxEarnPerTransaction = maxEarnPerTransaction
+        private set
+
+    var maxHoldingAmount: MaxHoldingAmount = maxHoldingAmount
+        private set
+
+    var defaultExpirationPeriod: DefaultExpirationPeriod = defaultExpirationPeriod
+        private set
+
+    fun update(
+        maxEarnPerTransaction: MaxEarnPerTransaction = this.maxEarnPerTransaction,
+        maxHoldingAmount: MaxHoldingAmount = this.maxHoldingAmount,
+        defaultExpirationPeriod: DefaultExpirationPeriod = this.defaultExpirationPeriod,
+    ) {
+        this.maxEarnPerTransaction = maxEarnPerTransaction
+        this.maxHoldingAmount = maxHoldingAmount
+        this.defaultExpirationPeriod = defaultExpirationPeriod
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PointPolicy) return false
+        return id == other.id
+    }
+
+    override fun hashCode(): Int = id.hashCode()
+}
