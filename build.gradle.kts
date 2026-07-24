09429b0f521dcdb94a9f403d49e81483fe1104e3
jys <jysohn0825@gmail.com>
2026-07-24 21:29:04 +0900

    refactor(domain): consolidate DefaultExpirationPeriod into ExpirationPeriod
    DefaultExpirationPeriod duplicated the same "1 day to under 5 years,
    default 365 days" rule that ExpirationPeriod already enforces, just
    with a different backing type. PointPolicy now reuses ExpirationPeriod
    so the rule lives in one place.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.plugin.spring) apply false
-    alias(libs.plugins.kotlin.plugin.jpa) apply false
-    alias(libs.plugins.kotlin.plugin.allopen) apply false
-    alias(libs.plugins.spring.boot) apply false
     alias(libs.plugins.ktlint) apply false
 }
 
