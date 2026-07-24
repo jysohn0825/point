77b951d482a1419d10c4bef19ca4208bd2985e99
jys <jysohn0825@gmail.com>
2026-07-24 20:13:44 +0900

    test(domain): EarningUsageTrace 테스트 및 픽스처 추가
    생성 조건, 동등성, copy 동작을 다른 line 값 객체 테스트와
    동일한 스타일로 검증

---
+++ domain/src/test/kotlin/com/jysohn0825/point/domain/vo/EarningUsageTraceTest.kt
@@ -0,0 +1,49 @@
+package com.jysohn0825.point.domain.vo
+
+import com.jysohn0825.point.domain.fixture.earningUsageTrace
+import com.jysohn0825.point.domain.fixture.orderNumber
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import java.math.BigDecimal
+
+class EarningUsageTraceTest :
+    FunSpec({
+        test("차감액이 0보다 크면 생성할 수 있다") {
+            val number = orderNumber()
+
+            val trace = EarningUsageTrace(number, BigDecimal(1_000))
+
+            trace.orderNumber shouldBe number
+            trace.amount shouldBe BigDecimal(1_000)
+        }
+
+        test("차감액이 0이면 예외가 발생한다") {
+            shouldThrow<IllegalArgumentException> { EarningUsageTrace(orderNumber(), BigDecimal.ZERO) }
+        }
+
+        test("차감액이 음수이면 예외가 발생한다") {
+            shouldThrow<IllegalArgumentException> { EarningUsageTrace(orderNumber(), BigDecimal(-1)) }
+        }
+
+        test("같은 값을 가진 EarningUsageTrace는 서로 동등하다") {
+            val number = orderNumber()
+
+            EarningUsageTrace(number, BigDecimal(1_000)) shouldBe EarningUsageTrace(number, BigDecimal(1_000))
+        }
+
+        test("다른 금액을 가진 EarningUsageTrace는 서로 동등하지 않다") {
+            val number = orderNumber()
+
+            (EarningUsageTrace(number, BigDecimal(1_000)) == EarningUsageTrace(number, BigDecimal(2_000))) shouldBe false
+        }
+
+        test("copy로 금액만 변경한 새 인스턴스를 만들 수 있다") {
+            val trace = earningUsageTrace(amount = BigDecimal(1_000))
+
+            val copied = trace.copy(amount = BigDecimal(500))
+
+            copied.orderNumber shouldBe trace.orderNumber
+            copied.amount shouldBe BigDecimal(500)
+        }
+    })
